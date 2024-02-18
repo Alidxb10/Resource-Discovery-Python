0d0239d82c842cc94a59367da98c8bbb61adc6c4
Alidxb10 <102414042+Alidxb10@users.noreply.github.com>
2024-02-18 11:10:30 +0400

    Update RD.py

--- RD.py
+++ RD.py
@@ -3,16 +3,14 @@ import requests
 #reading input file
 list1=[]
 list2=[]
-urls=open("recourses_G2.txt").readlines()
+#Upload a .txt File here which include all URLs You need to test them 
+urls=open("Upload-Your-TXT-File-Here").readlines()
 #cleaning data
 for i in urls:
     i=i.split("\n")
     list1.append(i[0])
-#print(list1)
-#for c in list1:
- #   r=requests.get(urls,auth=c)
-  #  list2.append(str(r.status_code))
-with open("Group2_recourse_discovery.txt","w") as file1:
+    # new File will be generated , so change the name of the file 
+with open("New-File","w") as file1:
     for k in list1:
         r=requests.get(k)
         file1.write(str(k)+str(r.status_code)+" "+"\n")
